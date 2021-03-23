7cf0ead9d5ccbd3535a0f17704a2e7009256dc4b
Mena Azir <AzirM6513@users.noreply.github.com>
2021-03-22 23:56:37 -0400

    add cors to server to any address

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 const Person = require("./models/person");
 
 app.use(express.json());
+app.use(cors());
 app.use(express.static("build"));
 app.use(morgan("tiny"));
 
